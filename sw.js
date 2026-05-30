a6f7e8e43ff4548517c7c1ec70706f9d45f082ce
BubbleDad <rfeigin@hutchingsbarsamian.com>
2026-05-30 05:29:06 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'meowmoon-bowling-v1-4-9-transparent-start-overlay-cache';
+const CACHE_NAME = 'meowmoon-bowling-v1-5-0-wording-cache';
 const CORE_ASSETS = [
   './',
   './index.html',
-  './app-v1.4.9.js',
+  './app-v1.5.0.js',
   './manifest.webmanifest',
   './assets/images/icon-192.png',
   './assets/images/icon-512.png',
